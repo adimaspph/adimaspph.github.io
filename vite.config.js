5d010acf3ac992e41bfa6776b090d330b8cf9867
adimaspph <adimas0101@gmail.com>
2022-06-23 04:24:46 +0700

    try deploy 2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "./",
+	base: "/adimaspph.github.io/",
 	publicPath:
 		process.env.NODE_ENV === "production" ? "/adimaspph.github.io/" : "/",
 	plugins: [vue()],
