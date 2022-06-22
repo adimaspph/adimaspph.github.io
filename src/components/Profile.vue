19ad836df440030a6a1668bc3b894e78403da374
adimaspph <adimas0101@gmail.com>
2022-06-23 03:31:12 +0700

    deployment test

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -9,7 +9,7 @@
             </div>
         </div>
         <div class="w-full self-end md:w-1/2 md:mt-0 md:right-0 relative">
-            <img class="rounded-xl max-w-full mx-auto md:right-0" src="../assets/editDSC02625 remove.png" alt="Foto Adimas Putra Pratama H">
+            <img class="rounded-xl max-w-full mx-auto md:right-0" src="../assets/editDSC02625_remove.png" alt="Foto Adimas Putra Pratama H">
             <span class="w-full h-full absolute top-16 letf-1/2 -z-10"> 
                 <svg class="w-full h-full" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
                     <path fill="#3b80f5" d="M43.1,-64.7C52.5,-52.6,54.3,-35.4,58.4,-19.7C62.5,-4,68.8,10.2,65.3,21.3C61.8,32.3,48.4,40.3,35.9,49.1C23.3,57.9,11.7,67.6,-0.7,68.6C-13.1,69.6,-26.2,61.8,-36.7,52.3C-47.3,42.9,-55.3,31.7,-60.3,19C-65.4,6.2,-67.4,-8.2,-62,-18.6C-56.5,-29.1,-43.6,-35.7,-32,-47.1C-20.5,-58.6,-10.2,-74.9,3.3,-79.5C16.9,-84.1,33.8,-76.9,43.1,-64.7Z" transform="translate(100 100)" />
