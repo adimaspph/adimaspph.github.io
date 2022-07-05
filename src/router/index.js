b95b7aca1b355a025c98ae1ab79bf88bb18fc71d
adimaspph <adimas0101@gmail.com>
2022-07-05 17:21:53 +0700

    update cv portfolio to netlify

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHistory, } from "vue-router"
 import ProjectBrives from "../pages/ProjectBrives.vue";
 import Home from "../pages/Home.vue"
 
