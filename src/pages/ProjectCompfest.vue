3d896363c052a7f0c62fba24237d68352eae65bd
adimaspph <adimas0101@gmail.com>
2022-07-07 23:05:11 +0700

    complete compfest project

--- src/pages/ProjectCompfest.vue
+++ src/pages/ProjectCompfest.vue
@@ -45,6 +45,101 @@
             <img src="../assets/projectCompfest/user-problem.jpg" alt="User Problem">
             <figcaption class="py-2 text-center text-sm italic">User Problem</figcaption>
         </div>
+        <p class="project-description">
+            Then we group the problem with affinity mapping.
+        </p>
+        <div class="project-img">
+            <img src="../assets/projectCompfest/affinity-mapping.jpg" alt="Affinity Mapping">
+            <figcaption class="py-2 text-center text-sm italic">Affinity Mapping</figcaption>
+        </div>
+        <h6 class="project-heading-2">Define Solution</h6>
+        <p class="project-description">
+            We define a solution based on the previous problem.
+        </p>
+        <div class="project-img">
+            <img src="../assets/projectCompfest/solution.jpg" alt="Solution">
+            <figcaption class="py-2 text-center text-sm italic">Solution Mapping</figcaption>
+        </div>
+
         <h6 class="project-heading-2">Create Persona</h6>
+        <p class="project-description">
+            We create personas that represent potential website users.
+        </p>
+        <div class="project-img">
+            <img src="../assets/projectCompfest/persona-1.jpg" alt="Persona 1">
+            <figcaption class="py-2 text-center text-sm italic">Persona 1</figcaption>
+        </div>
+        <div class="project-img">
+            <img src="../assets/projectCompfest/persona-2.jpg" alt="Persona 2">
+            <figcaption class="py-2 text-center text-sm italic">Persona 2</figcaption>
+        </div>
+
+        <h6 class="project-heading-2">Create User Flow</h6>
+        <p class="project-description">
+            We create a user flow for each main event.
+        </p>
+        <div class="project-img">
+            <img src="../assets/projectCompfest/user-flow.jpg" alt="User Flow for Job Fair Event">
+            <figcaption class="py-2 text-center text-sm italic">User Flow for Job Fair Event</figcaption>
+        </div>
+        
+        <h6 class="project-heading-2">Create Wireframe</h6>
+        <p class="project-description">
+            Then create a wireframe for each page. And don't forget to make a desktop and mobile version.
+        </p>
+        <div class="project-img">
+            <img src="../assets/projectCompfest/wireframe.jpg" alt="Wireframe for Jobfair Page">
+            <figcaption class="py-2 text-center text-sm italic">Wireframe for Jobfair Page</figcaption>
+        </div>
+
+        <h6 class="project-heading-2">Create Design System</h6>
+        <p class="project-description">
+            Then we make a design system together so that the design made is consistent for each page. The design system made includes color, typography, spacing, and components.
+        </p>
+        <div class="project-img">
+            <img src="../assets/projectCompfest/color-branding.jpg" alt="Color Branding">
+            <figcaption class="py-2 text-center text-sm italic">Color Branding</figcaption>
+        </div>
+        <div class="project-img">
+            <img src="../assets/projectCompfest/typography.jpg" alt="Typography and Font Rule">
+            <figcaption class="py-2 text-center text-sm italic">Typography and Font Rule</figcaption>
+        </div>
+        <div class="project-img">
+            <img src="../assets/projectCompfest/component.jpg" alt="Wireframe for Jobfair Page">
+            <figcaption class="py-2 text-center text-sm italic">Component</figcaption>
+        </div>
+
+        <h6 class="project-heading-2">Create High Fidelity Design</h6>
+        <p class="project-description">
+            After the design system and component is complete, each member makes a high-fidelity design according to the assigned task division.
+        </p>
+        <div class="project-img">
+            <img src="../assets/projectCompfest/jobfair-hifi.jpg" alt="Jobfair Page High-Fidelity Design">
+            <figcaption class="py-2 text-center text-sm italic">Jobfair Page High-Fidelity Design</figcaption>
+        </div>
+        <div class="project-img">
+            <img src="../assets/projectCompfest/application-form-hifi.jpg" alt="Application Form Page High-Fidelity Design">
+            <figcaption class="py-2 text-center text-sm italic">Application Form Page High-Fidelity Design</figcaption>
+        </div>
+        <div class="project-img">
+            <img src="../assets/projectCompfest/application-form-mobile-hifi.jpg" alt="Application Form Page for Mobile Version">
+            <figcaption class="py-2 text-center text-sm italic">Application Form Page for Mobile Version</figcaption>
+        </div>
+
+        <h6 class="project-heading-2">Testing</h6>
+        <p class="project-description">
+            First, we provide several tasks that users must carry out, such as applying for a job fair, taking quizzes, registering for webinars, and others. Next, the user is asked to do the task using the Figma prototype. While users do a job, users are asked to verbalize their thoughts as they move through the user interface to gather data (Thinking Aloud Method). After that, a user is asked to provide suggestions and complaints.
+        </p>
+        <div class="project-img">
+            <img src="../assets/projectCompfest/UT-deliverables.jpg" alt="UT Deliverables">
+            <figcaption class="py-2 text-center text-sm italic">UT Deliverables</figcaption>
+        </div>
+
+        <h5 class="project-heading">Result</h5>
+        <p class="project-description">
+            With some feedback and revisions from testing, the final design is finally done. Then the next step is the development process by Software Engineer Team.
+        </p>
+        <a class="w-fit" href="http://cf12.compfest.id/" target="_blank"><button class="my-4 button">Visit Compfest 12 Staging</button></a>
+
     </section>
 </template>
