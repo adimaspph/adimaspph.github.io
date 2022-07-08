29a26f4d820c2bd0736d7fa02902eb6758027a96
adimaspph <adimas0101@gmail.com>
2022-07-09 01:51:06 +0700

    add findterest project

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -20,16 +20,23 @@
                         <router-link exact to="/compfest" class="hover:opacity-80 transition-all">
                             <img class="my-4 rounded-sm" id="project1" src="../assets/projectCompfest/compfest-cover.jpg" alt="Compfest">
                         </router-link>
-                            <h5 class="block font-semibold text-ls" for="project1">COMPFEST Web Design</h5>
+                            <h5 class="block font-semibold text-lg" for="project1">COMPFEST Web Design</h5>
                             <p class="">COMPFEST is the biggest student-led IT event in Indonesia.</p>
                     </div>
-                    
+
                     <div class="w-full py-6 sm:px-6 md:w-1/2 ">
                         <router-link to="#" class="hover:opacity-80 transition-all">
                             <img class="my-4 rounded-sm" id="project1" src="../assets/projectEducare/educare-2021.jpg" alt="Educare">
                         </router-link>
-                            <h5 class="block font-semibold text-ls" for="project1">EDUCARE Web Design</h5>
+                            <h5 class="block font-semibold text-lg" for="project1">EDUCARE Web Design</h5>
                             <p class="">Educate is a series of events dedicated to facilitate and educate STEM university students in Jabodetabek.</p>
+                    </div>
+                    <div class="w-full py-6 sm:px-6 md:w-1/2 ">
+                        <a target="_blank" href="https://findterest-final.herokuapp.com/" class="hover:opacity-80 transition-all">
+                            <img class="my-4 rounded-sm" id="project1" src="../assets/projectFindterest/Findterest.jpg" alt="Educare">
+                        </a>
+                            <h5 class="block font-semibold text-lg" for="project1">Findterest | Web Application</h5>
+                            <p class="">Findterest, find friends according to your interests</p>
                     </div>
                 </div>
             </div>
