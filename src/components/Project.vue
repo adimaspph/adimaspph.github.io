256c588248000066c4b75011760164e28bf989d3
adimaspph <adimas0101@gmail.com>
2022-07-08 21:36:23 +0700

    add educare project

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -7,20 +7,29 @@
                     My Recent Project
                 </h2>
                 <!-- <p class="text-center text-base">This is a project that I have worked on. I hope you can know me more </p> -->
-                <div class="flex flex-wrap">
+                <div class="flex flex-wrap justify-center">
                     <div class="w-full py-6 sm:px-6 md:w-1/2">
                         <router-link exact to="/brives" class="hover:opacity-80 transition-all">
-                            <img class="my-4 rounded-sm" id="project1" src="../assets/project_brives.png" alt="Brive">
+                            <img class="my-4 rounded-sm" id="project1" src="../assets/project_brives.png" alt="Brives">
                         </router-link>
                             <h5 class="block font-semibold text-lg" for="project1">BRIVES, Online Private Tutoring Booking Website</h5>
                             <p class="">BRIVES is a website for students who want online private tutoring in BTA.</p>
                     </div>
+
                     <div class="w-full py-6 sm:px-6 md:w-1/2 ">
                         <router-link exact to="/compfest" class="hover:opacity-80 transition-all">
-                            <img class="my-4 rounded-sm" id="project1" src="../assets/projectCompfest/compfest-cover.jpg" alt="Brive">
+                            <img class="my-4 rounded-sm" id="project1" src="../assets/projectCompfest/compfest-cover.jpg" alt="Compfest">
                         </router-link>
                             <h5 class="block font-semibold text-ls" for="project1">COMPFEST Web Design</h5>
                             <p class="">COMPFEST is the biggest student-led IT event in Indonesia.</p>
+                    </div>
+                    
+                    <div class="w-full py-6 sm:px-6 md:w-1/2 ">
+                        <router-link to="#" class="hover:opacity-80 transition-all">
+                            <img class="my-4 rounded-sm" id="project1" src="../assets/projectEducare/educare-2021.jpg" alt="Educare">
+                        </router-link>
+                            <h5 class="block font-semibold text-ls" for="project1">EDUCARE Web Design</h5>
+                            <p class="">Educate is a series of events dedicated to facilitate and educate STEM university students in Jabodetabek.</p>
                     </div>
                 </div>
             </div>
